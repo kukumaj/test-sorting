5823e683eb37caae5539246137d024c6921c0edb
Ryszard <zakumaj@gmail.com>
2022-01-07 05:00:18 +0100

    first commit

--- src/main/java/com/epam/rd/autotasks/Sorting.java
+++ src/main/java/com/epam/rd/autotasks/Sorting.java
@@ -1,7 +1,13 @@
 package com.epam.rd.autotasks;
 
+import java.util.Arrays;
+
 public class Sorting {
     public void sort(int[] array){
-        throw new UnsupportedOperationException();
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        Arrays.sort(array);
+        //throw new UnsupportedOperationException();
     }
 }
--- src/main/java/com/epam/rd/autotasks/SortingTest.java
+++ src/main/java/com/epam/rd/autotasks/SortingTest.java
@@ -1,35 +1,50 @@
 package com.epam.rd.autotasks;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 
 public class SortingTest {
+     public  SortingTest() {this.sorting = new Sorting(); }
 
-    Sorting sorting = new Sorting();
-
-    @Test
-    public void testNullCase(){
-
+    //Sorting sorting = new Sorting();
+    Sorting sorting;
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullCase()throws Exception {
+    int [] array = null;
+    sorting.sort(array);
     }
 
-    @Test
-    public void testEmptyCase(){
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmptyCase() {
+        int [] array = {};
+        sorting.sort(array);   
 
     }
 
     @Test
     public void testSingleElementArrayCase() {
-
+     int [] expected ={1};
+     int [] given ={1};
+     sorting.sort(given);
+     Assert.assertArrayEquals(given,expected);
     }
 
     @Test
     public void testSortedArraysCase() {
-
+     int [] expected = {1,2,3,4,5};
+     int [] given = {5,3,2,4,1};
+     sorting.sort(given);
+     Assert.assertArrayEquals(given,expected);
     }
 
     @Test
     public void testOtherCases() {
-
+        int [] expected = {1,2,3,4,5};
+        int [] given = {5,3,2,4,1};
+        sorting.sort(given);
+        Assert.assertNotEquals(given,expected);
     }
 
 }
